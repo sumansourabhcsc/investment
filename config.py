4636ce84fa95d7150231d58131e8cd7bd4014b59
sumansourabhcsc <sumansourabhcsc@gmail.com>
2026-04-18 18:14:40 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,64 @@
 # config.py
 
 mutual_funds = {
-    "Axis Small Cap Fund": "125354",
-    "Bandhan Small Cap Fund": "147946",
-    "SBI Small Cap Fund": "125497",
-    "quant Small Cap Fund": "120828",
-    "Motilal Oswal Midcap Fund": "127042",
-    "HSBC Midcap Fund": "151034",
-    "Kotak Midcap Fund": "119775",
-    "quant Mid Cap Fund": "120841",
-    "Edelweiss Nifty Midcap150 Momentum 50 Index Fund": "150902",
-    "Parag Parikh Flexi Cap Fund": "122639",
-    "Kotak Flexicap Fund": "112090",
-    "Nippon India Large Cap Fund": "118632",
-    "ICICI Pru BHARAT 22 FOF": "143903",
-    "Mirae Asset FANG+": "148928",
-    "SBI Magnum Children's Benefit Fund": "148490"
+    "Axis Small Cap Fund": {
+        "code": "125354",
+        "folder": "axis_small_cap_fund"
+    },
+    "Bandhan Small Cap Fund": {
+        "code": "147946",
+        "folder": "bandhan_small_cap_fund"
+    },
+    "SBI Small Cap Fund": {
+        "code": "125497",
+        "folder": "sbi_small_cap_fund"
+    },
+    "quant Small Cap Fund": {
+        "code": "120828",
+        "folder": "quant_small_cap_fund"
+    },
+    "Motilal Oswal Midcap Fund": {
+        "code": "127042",
+        "folder": "motilal_oswal_midcap_fund"
+    },
+    "HSBC Midcap Fund": {
+        "code": "151034",
+        "folder": "hsbc_midcap_fund"
+    },
+    "Kotak Midcap Fund": {
+        "code": "119775",
+        "folder": "kotak_midcap_fund"
+    },
+    "quant Mid Cap Fund": {
+        "code": "120841",
+        "folder": "quant_midcap_fund"
+    },
+    "Edelweiss Nifty Midcap150 Momentum 50 Index Fund": {
+        "code": "150902",
+        "folder": "edelweiss_midcap150_momentum"
+    },
+    "Parag Parikh Flexi Cap Fund": {
+        "code": "122639",
+        "folder": "parag_parikh_flexicap_fund"
+    },
+    "Kotak Flexicap Fund": {
+        "code": "112090",
+        "folder": "kotak_flexicap_fund"
+    },
+    "Nippon India Large Cap Fund": {
+        "code": "118632",
+        "folder": "nippon_largecap_fund"
+    },
+    "ICICI Pru BHARAT 22 FOF": {
+        "code": "143903",
+        "folder": "icici_bharat22_fof"
+    },
+    "Mirae Asset FANG+": {
+        "code": "148928",
+        "folder": "mirae_fang_plus"
+    },
+    "SBI Magnum Children's Benefit Fund": {
+        "code": "148490",
+        "folder": "sbi_childrens_fund"
+    }
 }
