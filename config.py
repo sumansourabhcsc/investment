dbe068da418e282c4dd31b54721e1dd6059699a5
sumansourabhcsc <sumansourabhcsc@gmail.com>
2026-04-18 18:01:25 +0200

    Add mutual funds configuration in config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# config.py
+
+mutual_funds = {
+    "Axis Small Cap Fund": "axis_small_cap_fund",
+    "Bandhan Small Cap Fund": "bandhan_small_cap_fund",
+    "SBI Small Cap Fund": "sbi_small_cap_fund",
+    "quant Small Cap Fund": "quant_small_cap_fund",
+    "Motilal Oswal Midcap Fund": "motilal_oswal_midcap_fund",
+    "HSBC Midcap Fund": "hsbc_midcap_fund",
+    "Kotak Midcap Fund": "kotak_midcap_fund",
+    "quant Mid Cap Fund": "quant_midcap_fund",
+    "Edelweiss Nifty Midcap150 Momentum 50 Index Fund": "edelweiss_midcap150_momentum",
+    "Parag Parikh Flexi Cap Fund": "parag_parikh_flexicap_fund",
+    "Kotak Flexicap Fund": "kotak_flexicap_fund",
+    "Nippon India Large Cap Fund": "nippon_largecap_fund",
+    "ICICI Pru BHARAT 22 FOF": "icici_bharat22_fof",
+    "Mirae Asset FANG+": "mirae_fang_plus",
+    "SBI Magnum Children's Benefit Fund": "sbi_childrens_fund"
+}
