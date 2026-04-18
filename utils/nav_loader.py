f40a6810effa763fb53bb9e8e7280ff4afd1b801
sumansourabhcsc <sumansourabhcsc@gmail.com>
2026-04-18 05:49:23 +0200

    Update nav_loader.py

--- utils/nav_loader.py
+++ utils/nav_loader.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+# this will load the daily NAV file to dataframe- df
+def load_nav_file(nav_path="data/nav_all_latest.txt"):
+    df = pd.read_csv(nav_path, sep=";")
+    df["Date"] = pd.to_datetime(df["Date"], format="%d-%b-%Y")
+    df["SchemeCode"] = df["SchemeCode"].astype(str)
+    return df
