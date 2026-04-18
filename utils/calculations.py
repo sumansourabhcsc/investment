1038b6bd37e4d595414f7f958c4df6d208576691
sumansourabhcsc <sumansourabhcsc@gmail.com>
2026-04-18 06:39:31 +0200

    Update calculations.py

--- utils/calculations.py
+++ utils/calculations.py
@@ -53,3 +53,26 @@ def calculate_summary(df):
 
     return summary
 
+
+import numpy as np
+from datetime import datetime
+
+def xirr(cashflows, dates, guess=0.1):
+    """Compute XIRR using Newton's method."""
+    def npv(rate):
+        return sum(cf / ((1 + rate) ** ((d - dates[0]).days / 365)) for cf, d in zip(cashflows, dates))
+
+    rate = guess
+    for _ in range(100):
+        f = npv(rate)
+        df = sum(
+            -((d - dates[0]).days / 365) * cf / ((1 + rate) ** (((d - dates[0]).days / 365) + 1))
+            for cf, d in zip(cashflows, dates)
+        )
+        new_rate = rate - f / df
+        if abs(new_rate - rate) < 1e-7:
+            return new_rate
+        rate = new_rate
+
+    return rate
+
