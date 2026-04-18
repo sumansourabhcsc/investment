04621831e3260806e2c6ecdc39c62c466b490c8f
sumansourabhcsc <sumansourabhcsc@gmail.com>
2026-04-18 17:53:53 +0200

    Update calculations.py

--- utils/calculations.py
+++ utils/calculations.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+def calculate_current_value(fund_df, latest_nav):
+    total_units = fund_df["Units"].sum()
+    return total_units * latest_nav
+
+
+def calculate_invested_amount(fund_df):
+    return fund_df["Amount"].sum()
+
+
+def calculate_profit(invested, current):
+    return current - invested
