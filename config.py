13020a39a9cff31615434630fc736a3d79df3803
sumansourabhcsc <sumansourabhcsc@gmail.com>
2026-04-18 18:10:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 # config.py
 
 mutual_funds = {
-    "Axis Small Cap Fund": "axis_small_cap_fund",
-    "Bandhan Small Cap Fund": "bandhan_small_cap_fund",
-    "SBI Small Cap Fund": "sbi_small_cap_fund",
-    "quant Small Cap Fund": "quant_small_cap_fund",
-    "Motilal Oswal Midcap Fund": "motilal_oswal_midcap_fund",
-    "HSBC Midcap Fund": "hsbc_midcap_fund",
-    "Kotak Midcap Fund": "kotak_midcap_fund",
-    "quant Mid Cap Fund": "quant_midcap_fund",
-    "Edelweiss Nifty Midcap150 Momentum 50 Index Fund": "edelweiss_midcap150_momentum",
-    "Parag Parikh Flexi Cap Fund": "parag_parikh_flexicap_fund",
-    "Kotak Flexicap Fund": "kotak_flexicap_fund",
-    "Nippon India Large Cap Fund": "nippon_largecap_fund",
-    "ICICI Pru BHARAT 22 FOF": "icici_bharat22_fof",
-    "Mirae Asset FANG+": "mirae_fang_plus",
-    "SBI Magnum Children's Benefit Fund": "sbi_childrens_fund"
+    "Axis Small Cap Fund": "125354",
+    "Bandhan Small Cap Fund": "147946",
+    "SBI Small Cap Fund": "125497",
+    "quant Small Cap Fund": "120828",
+    "Motilal Oswal Midcap Fund": "127042",
+    "HSBC Midcap Fund": "151034",
+    "Kotak Midcap Fund": "119775",
+    "quant Mid Cap Fund": "120841",
+    "Edelweiss Nifty Midcap150 Momentum 50 Index Fund": "150902",
+    "Parag Parikh Flexi Cap Fund": "122639",
+    "Kotak Flexicap Fund": "112090",
+    "Nippon India Large Cap Fund": "118632",
+    "ICICI Pru BHARAT 22 FOF": "143903",
+    "Mirae Asset FANG+": "148928",
+    "SBI Magnum Children's Benefit Fund": "148490"
 }
