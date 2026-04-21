fb28190c88921237973d7cbc194ac13c1776486b
sumansourabhcsc <sumansourabhcsc@gmail.com>
2026-04-21 09:00:43 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,37 @@
 # config.py
 
 mutual_funds = {
-    "Axis Small Cap Fund": {
-        "code": "125354",
-        "folder": "axis_small_cap_fund"
+
+    "Mirae Asset FANG+": {
+        "code": "148928",
+        "folder": "mirae_fang_plus"
+    },
+     "SBI Magnum Children's Benefit Fund": {
+        "code": "148490",
+        "folder": "sbi_childrens_fund"
     },
     "Bandhan Small Cap Fund": {
         "code": "147946",
         "folder": "bandhan_small_cap_fund"
     },
+    "Motilal Oswal Midcap Fund": {
+        "code": "127042",
+        "folder": "motilal_oswal_midcap_fund"
+    },
+    "Parag Parikh Flexi Cap Fund": {
+        "code": "122639",
+        "folder": "parag_parikh_flexicap_fund"
+    },
+    "Nippon India Large Cap Fund": {
+        "code": "118632",
+        "folder": "nippon_largecap_fund"
+    },
+    
+    "Axis Small Cap Fund": {
+        "code": "125354",
+        "folder": "axis_small_cap_fund"
+    },
+    
     "SBI Small Cap Fund": {
         "code": "125497",
         "folder": "sbi_small_cap_fund"
@@ -17,10 +40,7 @@ mutual_funds = {
         "code": "120828",
         "folder": "quant_small_cap_fund"
     },
-    "Motilal Oswal Midcap Fund": {
-        "code": "127042",
-        "folder": "motilal_oswal_midcap_fund"
-    },
+    
     "HSBC Midcap Fund": {
         "code": "151034",
         "folder": "hsbc_midcap_fund"
@@ -37,28 +57,16 @@ mutual_funds = {
         "code": "150902",
         "folder": "edelweiss_midcap150_momentum"
     },
-    "Parag Parikh Flexi Cap Fund": {
-        "code": "122639",
-        "folder": "parag_parikh_flexicap_fund"
-    },
+    
     "Kotak Flexicap Fund": {
         "code": "112090",
         "folder": "kotak_flexicap_fund"
     },
-    "Nippon India Large Cap Fund": {
-        "code": "118632",
-        "folder": "nippon_largecap_fund"
-    },
+    
     "ICICI Pru BHARAT 22 FOF": {
         "code": "143903",
         "folder": "icici_bharat22_fof"
-    },
-    "Mirae Asset FANG+": {
-        "code": "148928",
-        "folder": "mirae_fang_plus"
-    },
-    "SBI Magnum Children's Benefit Fund": {
-        "code": "148490",
-        "folder": "sbi_childrens_fund"
     }
+    
+   
 }
