9dd624b595ab91a3b9858a76725781bb132670fd
sumansourabhcsc <sumansourabhcsc@gmail.com>
2026-04-25 06:11:31 +0200

    Update fetch_nav_all.py

--- utils/fetch_nav_all.py
+++ utils/fetch_nav_all.py
@@ -11,21 +11,22 @@ OUTPUT_FILE = f"{OUTPUT_DIR}/nav_all_latest.csv"
 # YOUR FILTER LIST (EASY TO UPDATE)
 # -----------------------------
 mutual_funds = {
+    "Mirae Asset FANG+": "148928",
+    "SBI Magnum Children's Benefit Fund": "148490",
+    "Parag Parikh Flexi Cap Fund": "122639",
     "Bandhan Small Cap Fund": "147946",
+    "Motilal Oswal Midcap Fund": "127042",
+    "Nippon India Large Cap Fund": "118632",
     "Axis Small Cap Fund": "125354",
     "SBI Small Cap Fund": "125497",
     "quant Small Cap Fund": "120828",
-    "Motilal Oswal Midcap Fund": "127042",
     "HSBC Midcap Fund": "151034",
     "Kotak Midcap Fund": "119775",
     "quant Mid Cap Fund": "120841",
     "Edelweiss Nifty Midcap150 Momentum 50 Index Fund": "150902",
-    "Parag Parikh Flexi Cap Fund": "122639",
     "Kotak Flexicap Fund": "112090",
-    "Nippon India Large Cap Fund": "118632",
-    "ICICI Pru BHARAT 22 FOF": "143903",
-    "Mirae Asset FANG+": "148928",
-    "SBI Magnum Children's Benefit Fund": "148490"
+    "ICICI Pru BHARAT 22 FOF": "143903"
+    
 }
 
 # Convert to set for fast lookup
