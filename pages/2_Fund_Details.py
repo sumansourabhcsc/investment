e05c52920cfb3796ca52a108ac4da48e69cc2d3a
sumansourabhcsc <sumansourabhcsc@gmail.com>
2026-05-10 12:35:13 +0200

    Update 2_Fund_Details.py

--- pages/2_Fund_Details.py
+++ pages/2_Fund_Details.py
@@ -375,7 +375,7 @@ with col_tab:
 def fetch_nav_history(fund_code):
     url = f"https://api.mfapi.in/mf/{fund_code}"
     try:
-        response = requests.get(url, timeout=10)
+        response = requests.get(url, timeout=30)
         response.raise_for_status()
         data = response.json()
         if "data" in data and data["data"]:
