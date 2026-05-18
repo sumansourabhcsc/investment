d1c780e9856dc1be2d066bbecb6f496892763f9f
sumansourabhcsc <sumansourabhcsc@gmail.com>
2026-05-18 23:21:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,86 +5,102 @@ mutual_funds = {
     "Mirae Asset FANG+": {
         "code": "148928",
 		"folio": "79938977986",
+		"category": "International",
         "folder": "mirae_fang_plus"
     },
      "SBI Magnum Children's Benefit Fund": {
         "code": "148490",
 		"folio": "43682993",
+		 "category": "Hybrid",
         "folder": "sbi_childrens_fund"
     },
     "Bandhan Small Cap Fund": {
         "code": "147946",
 		"folio": "7429522",
+		"category": "Small Cap",
         "folder": "bandhan_small_cap_fund"
     },
     "Motilal Oswal Midcap Fund": {
         "code": "127042",
 		"folio": "6910146429673",
+		"category": "Mid Cap",
         "folder": "motilal_oswal_midcap_fund"
     },
     "Edelweiss Flexi Cap Fund": {
         "code": "140353",
 	    "folio": "91047811036",
+		"category": "Flexi Cap",
         "folder": "edelweiss_flexi_cap_fund"
     },
     "Parag Parikh Flexi Cap Fund": {
         "code": "122639",
 		"folio": "10944223, 18757062",
+		"category": "Flexi Cap",
         "folder": "parag_parikh_flexicap_fund"
     },
     "Nippon India Large Cap Fund": {
         "code": "118632",
 		"folio": "499446784807",
+		"category": "Large Cap",
         "folder": "nippon_largecap_fund"
     },
     
     "Axis Small Cap Fund": {
         "code": "125354",
 		"folio": "910100034824",
+		"category": "Small Cap",
         "folder": "axis_small_cap_fund"
     },
     
     "SBI Small Cap Fund": {
         "code": "125497",
 		"folio": "28621020",
+		"category": "Small Cap",
         "folder": "sbi_small_cap_fund"
     },
     "quant Small Cap Fund": {
         "code": "120828",
 		"folio": "51011322275",
+		"category": "Small Cap",
         "folder": "quant_small_cap_fund"
     },
     
     "HSBC Midcap Fund": {
         "code": "151034",
 		"folio": "3543668",
+		"category": "Mid Cap",
         "folder": "hsbc_midcap_fund"
     },
     "Kotak Midcap Fund": {
         "code": "119775",
 		"folio": "10368494",
+		"category": "Mid Cap",
         "folder": "kotak_midcap_fund"
     },
     "quant Mid Cap Fund": {
         "code": "120841",
 		"folio": "51011322275",
+		"category": "Mid Cap",
         "folder": "quant_midcap_fund"
     },
     "Edelweiss Nifty Midcap150 Momentum 50 Index Fund": {
         "code": "150902",
 		"folio": "9106300903",
+		"category": "Mid Cap",
         "folder": "edelweiss_midcap150_momentum"
     },
     
     "Kotak Flexicap Fund": {
         "code": "112090",
 		"folio": "4681104",
+		"category": "Flexi Cap",
         "folder": "kotak_flexicap_fund"
     },
     
     "ICICI Pru BHARAT 22 FOF": {
         "code": "143903",
 		"folio": "16393741",
+		"category": "Large Cap",
         "folder": "icici_bharat22_fof"
     }
     
