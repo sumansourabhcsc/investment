8192108059abc030d11ae389b94341bbd6717bf4
sumansourabhcsc <sumansourabhcsc@gmail.com>
2026-05-28 05:45:53 +0200

    Update 1_Portfolio_Overview.py

--- pages/1_Portfolio_Overview.py
+++ pages/1_Portfolio_Overview.py
@@ -431,7 +431,7 @@ with tab1:
         </html>
         """
     
-        components.html(table_html, height=(len(display_df) + 2) * 52, scrolling=False)
+        components.html(table_html, height=(len(display_df) + 1) * 52, scrolling=False)
 
     with col_donut:
         fig_donut = go.Figure(go.Pie(
