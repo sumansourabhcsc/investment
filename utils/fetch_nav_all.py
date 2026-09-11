ad5c3b48c88c1024a2749c5d27f35b5f39af2829
sumansourabhcsc <sumansourabhcsc@gmail.com>
2026-09-11 22:46:35 +0200

    Remove 'quant Mid Cap Fund' from fund list
    Removed 'quant Mid Cap Fund' entry from the fund list.

--- utils/fetch_nav_all.py
+++ utils/fetch_nav_all.py
@@ -24,7 +24,6 @@ mutual_funds = {
     "quant Small Cap Fund": "120828",
     "HSBC Midcap Fund": "151034",
     "Kotak Midcap Fund": "119775",
-    "quant Mid Cap Fund": "120841",
     "Edelweiss Nifty Midcap150 Momentum 50 Index Fund": "150902",
     "Kotak Flexicap Fund": "112090",
     "ICICI Pru BHARAT 22 FOF": "143903"
