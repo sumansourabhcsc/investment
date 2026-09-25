ecf28f18f2569833637c601ec871a91f1bdb33c7
sumansourabhcsc <sumansourabhcsc@gmail.com>
2026-09-25 15:39:45 +0200

    Remove Edelweiss Flexi Cap Fund from navigation
    Removed 'Edelweiss Flexi Cap Fund' from the list.

--- utils/fetch_nav_all.py
+++ utils/fetch_nav_all.py
@@ -16,7 +16,6 @@ mutual_funds = {
     "SBI Magnum Children's Benefit Fund": "148490",
     "Parag Parikh Flexi Cap Fund": "122639",
     "Bandhan Small Cap Fund": "147946",
-    "Edelweiss Flexi Cap Fund": "140353",
     "Motilal Oswal Midcap Fund": "127042",
     "Nippon India Large Cap Fund": "118632",
     "Axis Small Cap Fund": "125354",
